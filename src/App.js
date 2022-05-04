8650986b5fe086c2b08be3cd048f93b09a870318
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2022-05-04 16:49:28 -0400

    Begin working on form content

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
 import React from "react";
+import InputZip from "./comps/InputZip";
+
+function App() {
+
+
+    return (
+        
+        <div>
+            <nav className="nav-container">
+                <a className="nav-item" href="/">Weather</a>
+                <a className="nav-item" href="/">About</a>
+                <a className="nav-item" href="/">Contact</a>
+            </nav>
+
+            <InputZip />
+        </div>
+    );
+
+}
 
 export default App;
---
+++ src/comps/InputZip.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const InputZip = () => {
+
+    const [zip, setZip] = useState(null)
+
+    return (
+
+        <div className="input-zip-container">
+            <p>Input Zip Code:</p>
+            <form>
+                <label for="zipcode"></label>
+                <input type="text" /> 
+            </form>
+        </div>
+    );
+
+}
+
+export default InputZip;
