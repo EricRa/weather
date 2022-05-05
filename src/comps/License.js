9180c3865448f3cb09d92332ff20a9f245f74224
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2022-05-04 21:16:29 -0400

    Fixed CSS margin issues

--- src/comps/License.js
+++ src/comps/License.js
@@ -5,7 +5,7 @@ const License = () => {
 
     return (
         <div className="license-content">
-            <p>All code for this page is available under the MIT license on <a href="https://github.com/EricRa/weather">Github</a>, and all content is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International</a> (CC BY-SA 4.0)</p>
+            <p className="license-text">All code for this page is available under the MIT license on <a href="https://github.com/EricRa/weather">Github</a>, and all content is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International</a> (CC BY-SA 4.0)</p>
         </div>
     );
 }
