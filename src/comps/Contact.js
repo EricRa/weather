7014b91f7e77b6c82f561e11c754cc67b88ebffd
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2022-05-05 01:31:49 -0400

    Working on contact info and CSS

--- src/comps/Contact.js
+++ src/comps/Contact.js
@@ -6,6 +6,9 @@ const Contact = () => {
     return (
         <div className="contact-content">
             <p>Contact me here:</p>
+            <p>E-mail: eric @ ericra.com </p>
+            <p>Github: <a href="https://github.com/EricRa/">Ericra</a></p>
+            <p>Twitter: </p>
         </div>
     );
 }
