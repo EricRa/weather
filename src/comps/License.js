d2b69b4b90c2eb1c0176d720733814f3b1cb2e15
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2022-05-06 20:55:40 -0400

    Start working on API call and netlify function

--- src/comps/License.js
+++ src/comps/License.js
@@ -5,7 +5,7 @@ const License = () => {
 
     return (
         <div className="license-content">
-            <p className="license-text">All code for this page is available under the MIT license on <a href="https://github.com/EricRa/weather" target="_blank" rel="noopener noreferrer">Github</a>, and all content is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener noreferrer">Creative Commons Attribution-ShareAlike 4.0 International</a> (CC BY-SA 4.0).</p>
+            <p className="license-text">All code for this page is available under the MIT license on <a href="https://github.com/EricRa/weather" target="_blank" rel="noopener noreferrer">Github</a>, and all other content is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener noreferrer">Creative Commons Attribution-ShareAlike 4.0 International</a> (CC BY-SA 4.0).</p>
         </div>
     );
 }
